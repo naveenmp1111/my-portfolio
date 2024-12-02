3ecf2b6980456dac8cfe0575c002c0ccdb5e89c6
Naveen <naveenapk048@gmail.com>
2024-12-02 20:16:16 +0530

    included background spotlightand magic button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     return (
         <div className='pb-20 pt-36'>
             <div>
-                {/* <Spotlight className='left-0 top-0 h-screen' fill='white' /> */}
+                <Spotlight className='left-0 top-0 h-screen' fill='white' />
             </div>
 
             <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.3] flex items-center justify-center absolute top-0 left-0">
@@ -18,13 +18,13 @@ const Hero = () => {
             <div className='flex justify-center relative my-20 z-10'>
                 <div className='max-w-[89vw] flex flex-col'>
                     <h2 className='uppercase tracking-widest text-sm text-center text-blue-100'>Hello from the other side</h2>
-                    {/* <TextGenerateEffect
+                    <TextGenerateEffect
                         className='text-center text-[40px] md:text-5xl lg:text-6xl'
                         words='My pants velour, my shades dior. Create Explore Expand Conquer.'
-                    /> */}
+                    />
                     <p className='text-center tracking-widest break-words'>I'm Cristiano Ronaldo, a professional Football player.</p>
                     <a href='#about' className='flex justify-center md:mt-10 mt-5'>
-                       {/* <MagicButton title='Click Here'/> */}
+                       <MagicButton title='Click Here'/>
                     </a>
                 </div>
             </div>
